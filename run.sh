451b8fd98efff56c33da3cb3a67dbd05c16fcc2e
Maxime David <maxime.david@datadoghq.com>
2023-11-11 13:48:56 -0500

    updateReadme

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 SECRET=$1
-SUFFIX=$(tr -dc a-z </dev/urandom | head -c 20 ; echo '')
 if [ -z "${SECRET}" ]; then
     echo "Please provide a secret"
     exit 1
 fi
-echo "Using secret: ${SECRET}"
-docker run -it --rm -v $(pwd):/app -e SECRET=${SECRET} -e SUFFIX=${SUFFIX} maxdayworkshop/rust-workshop
+docker run -it --rm -v $(pwd):/app -e SECRET=${SECRET} maxdayworkshop/rust-workshop
